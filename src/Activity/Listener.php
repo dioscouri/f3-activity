e1a8fcdb181fd994ce7ccadf384b615da752026f
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-05-27 23:27:26 -0400

    fixed menu items

--- src/Activity/Listener.php
+++ src/Activity/Listener.php
@@ -1,47 +1,44 @@
-<?php 
+<?php
 namespace Activity;
 
-class Listener extends \Prefab 
+class Listener extends \Prefab
 {
-    public function onSystemRebuildMenu( $event )
+
+    public function onSystemRebuildMenu($event)
     {
         if ($model = $event->getArgument('model'))
-        {  
-            $root = $event->getArgument( 'root' );
-            $modules = clone $model;
-        
-            $modules->insert(
-                    array(
-                            'type'  => 'admin.nav',
-                            'priority' => 200,
-                            'title' => 'Activity',
-                            'icon'  => 'fa fa-building',
-                            'is_root' => false,
-                            'tree'  => $root,
-                            'base' => '/admin/activity',
-                    )
-            );
+        {
+            $root = $event->getArgument('root');
+            $activity = clone $model;
+            
+            $activity->insert(array(
+                'type' => 'admin.nav',
+                'priority' => 150,
+                'title' => 'Activity',
+                'icon' => 'fa fa-bolt',
+                'is_root' => false,
+                'tree' => $root,
+                'base' => '/admin/activity'
+            ));
             
             $children = array(
-                    array( 'title'=>'List', 'route'=>'/admin/activity', 'icon'=>'fa fa-list' ),
-                    array( 'title'=>'Add New', 'route'=>'/admin/activity/create', 'icon'=>'fa fa-plus' ),
+                array(
+                    'title' => 'List',
+                    'route' => '/admin/activity',
+                    'icon' => 'fa fa-bar-chart-o'
+                ),
             );
-            $modules->addChildrenItems( $children, $root );
+            
+            $activity->addChildren($children, $root);
             
             \Dsc\System::instance()->addMessage('Activity added its admin menu items.');
         }
     }
 
-     public function afterSaveUsersModelsUsers( $event )
+    public function afterSaveUsersModelsUsers($event)
     {
-
-        $doc =  $event->getArgument('model');
-
-        (new \Activity\Models\Activity)->track('save', 'test',  $doc->cast());
-       
+        $doc = $event->getArgument('model');
+        
+        (new \Activity\Models\Activity())->track('save', 'test', $doc->cast());
     }
-
-
-
-    
 }
