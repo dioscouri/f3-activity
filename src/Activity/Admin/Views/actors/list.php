d1a19f66365081e29f6fa9cdff387a16bbd21f5f
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-07-14 10:57:25 -0400

    fixed title of page

--- src/Activity/Admin/Views/actors/list.php
+++ src/Activity/Admin/Views/actors/list.php
@@ -2,7 +2,7 @@
 	<div class="col-xs-12 col-sm-7 col-md-7 col-lg-4">
 		<h1 class="page-title txt-color-blueDark">
 			<i class="fa fa-table fa-fw "></i> 
-				Actions 
+				Actors 
 			<span> > 
 				List
 			</span>
